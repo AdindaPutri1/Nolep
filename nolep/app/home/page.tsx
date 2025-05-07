01f18a4025bb036966407ea4c658d2891b279a37
Drafaund <faundrapratamasukma505520@mail.ugm.ac.id>
2025-05-07 18:48:20 +0700

    fix: fixxing some errors

--- nolep/app/home/page.tsx
+++ nolep/app/home/page.tsx
@@ -1,16 +1,20 @@
 "use client";
+import ChartSection from "@/components/ui/ChartSection";
 import Navbar from "../../components/ui/Navbar";
 import dynamic from "next/dynamic";
 
-export default function function Home() {
+export default function Home() {
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
-          <h1 className="text-4xl font-bold text-center mt-20">Driver Drowsiness Monitor</h1>
-          <ChartSection />
+        <h1 className="text-4xl font-bold text-center mt-20">
+          Driver Drowsiness Monitor
+        </h1>
+        <ChartSection />
       </main>
     </>
   );
+}
