83e661586f23bf67da049898bf88c971a62bec9d
Drafaund <faundrapratamasukma505520@mail.ugm.ac.id>
2025-05-07 17:04:18 +0700

    feat: integrate prisma with clerk

--- nolep/app/home/page.tsx
+++ nolep/app/home/page.tsx
@@ -1,13 +1,16 @@
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "../../components/ui/Navbar";
 
-export default function Home() {
-    return (
-        <div>
-            <Navbar />
-            <main className="p-6">
-                <h1 className="text-2xl font-bold">Home</h1>
-                <p>Selamat datang di beranda aplikasi Nolep!</p>
-            </main>
-        </div>
-    );
+function Home() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <h1>Selamat datang di aplikasi Nolep</h1>
+      </main>
+    </>
+  );
 }
+
+export default Home;
--- nolep/app/page.tsx
+++ nolep/app/page.tsx
@@ -1,8 +1,19 @@
-"use client";
-import SigninForm from "@/components/pages/SigninForm";
+import { RedirectToSignIn, SignedOut, SignedIn } from "@clerk/nextjs";
+import Home from "@/app/home/page";
+import prisma from "@/lib/db";
 
-export default function Page() {
-    return (
-            <SigninForm />
-    );
+export default async function Page() {
+  const users = await prisma.user.findMany();
+  console.log(users); // Log the users to the console
+  return (
+    <>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+
+      <SignedIn>
+        <Home />
+      </SignedIn>
+    </>
+  );
 }
