98050c92cb63e787d21e8ba83054bcb4b1c5f59c
Adinda Putri Romadhon <114839413+AdindaPutri1@users.noreply.github.com>
2026-05-05 14:44:17 +0700

    Update page.tsx

--- nolep/app/page.tsx
+++ nolep/app/page.tsx
@@ -1,10 +1,7 @@
 import { RedirectToSignIn, SignedOut, SignedIn } from "@clerk/nextjs";
 import Home from "@/app/home/page";
-import prisma from "@/lib/db";
 
 export default async function Page() {
-  const users = await prisma.user.findMany();
-  console.log(users); // Log the users to the console
   return (
     <>
       <SignedOut>
